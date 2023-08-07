e69d5d2eae0a0bc077217da2a1838b02cc816dc5
TeacherHuang <40095644+TeacherHuang@users.noreply.github.com>
2023-08-07 16:44:18 +0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_dyp0fXi6BxAJ-6Rj3LV-c3htjDkybW6s6DpSr_wcRA/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1NyzSeHDXD07mfjDbHWw884iPn1ZpF7ZALTonAXrYdBM/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyB3y7vY4JjMmH68SjY9s78YVLo6V44uWVU';
